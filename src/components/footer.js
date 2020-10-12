7bbe827980e190f3459a3d043652f43d04d1853c
Jedd Fenner <jedd@diff.uk>
2020-10-12 09:27:02 +0100

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const Footer = ({ name, address, companyNumber }) => (
+  <>
+  <Row>
+    <a href="https://ecologi.com/diff" target="_blank" rel="noopener noreferrer" title="View our Ecologi profile" style="width:200px;display:inline-block;">
+      <img alt="We offset our carbon footprint via Ecologi" src="https://api.ecologi.com/badges/cpw/5e3fed1c59ec630016e27dd7?black=true&landscape=true" style="width:200px;" />
+    </a>
+  </Row>
   <Row>
     <footer>
       {name} <Em>|</Em>{" "}
@@ -11,6 +17,7 @@ const Footer = ({ name, address, companyNumber }) => (
       <Em>|</Em> #{companyNumber}
     </footer>
   </Row>
+  </>
 );
 
 const Em = styled.em`
